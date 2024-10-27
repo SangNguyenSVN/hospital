d2a1164352fbde1594f6b955291b9ad0cb10974e
SangNguyenSVN <nguyenvusang1108@gmail.com>
2024-10-27 13:55:18 +0700

    'sang'

--- app/src/components/shared/Button/LogoutButton.tsx
+++ app/src/components/shared/Button/LogoutButton.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import auth from '@/app/src/services/auth'; // Update this path to where your auth service is located
 
 const LogoutButton = () => {
    
 
     const handleLogout = () => {
-        auth.logout();
        
     };
 
